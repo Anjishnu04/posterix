d4a52540cfb6d62d1a56c8a9ff5a1765f5616bb4
Anjishnu04 <anjishnusarkar30@gmail.com>
2026-01-16 11:03:45 +0530

    initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './app.tsx'
 import './style/index.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/posterix">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
